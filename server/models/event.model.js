4d193792c94aa7a657568bce0ffaa3a07cbab8ea
tony <48603440+antonybstack@users.noreply.github.com>
2021-04-16 01:31:05 -0400

    create events functional

--- server/models/event.model.js
+++ server/models/event.model.js
@@ -2,36 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let Event = new Schema({
-    title: {
-        type: String,
-        require: true,
-        min: 1,
-        max: 128,
-    },
-    description: {
-        type: String,
-        require: true,
-        min: 1,
-        max: 2000,
-    },
-    location: [{
-        latitude: {
-            type: Number,
-            require: true,
-        },
-        longitude: {
-            type: Number,
-            require: true,
-        },
-    }],
-    eventDate: {
-        type: Date,
-        require: true,
-    },
-    creator: {
-        type: String,
-        require: true,
-    },
+  title: {
+    type: String,
+    require: true,
+    min: 1,
+    max: 128,
+  },
+  description: {
+    type: String,
+    require: true,
+    min: 1,
+    max: 2000,
+  },
+  location: {
+    type: [Number],
+    require: true,
+  },
+  eventDate: {
+    type: Date,
+    require: true,
+  },
+  creator: {
+    type: String,
+    require: true,
+  },
 });
 
-module.exports = mongoose.model("Event", Event)
+module.exports = mongoose.model("Event", Event);
--- server/routes/api/events.js
+++ server/routes/api/events.js
@@ -6,8 +6,17 @@ const passportconfig = require("../../passport-google");
 const Event = require("../../models/event.model");
 
 eventRoutes.post("/new", (req, res) => {
-    //mongodb statement to add new event
-    res.send("hello");
+  let event = new Event(req.body);
+  event
+    .save()
+    .then((event) => {
+      console.log(event);
+      res.status(200).json({ event });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400);
+    });
 });
 
 module.exports = eventRoutes;
