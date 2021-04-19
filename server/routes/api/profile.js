a604d01dbe2ef3af4d5eef5f80e8d3ff41788241
Matthew Rossi <12902926+MattRossi@users.noreply.github.com>
2021-04-19 19:15:32 -0400

    Made profile generic

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -2,11 +2,10 @@ const express = require("express");
 const profileRoutes = express.Router();
 const User = require("../../models/user.model");
 
-profileRoutes.get("/spec", (req, res) => {
+profileRoutes.post("/spec", (req, res) => {
 
     User.db.collections.users.find({googleId: req.body.googleId}).toArray((error, doc) => {
-        console.log(req);
-        res.status(200).json(doc);
+        res.status(200).json(doc[0]);
     });
   });
 
