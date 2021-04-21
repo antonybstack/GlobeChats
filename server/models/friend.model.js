248205cf97e8a28c0f78f10bc81f69b7d10f8ee1
Matthew Rossi <12902926+MattRossi@users.noreply.github.com>
2021-04-20 21:58:35 -0400

    friend(ly) fixes

--- server/models/friend.model.js
+++ server/models/friend.model.js
@@ -3,17 +3,9 @@ const Schema = mongoose.Schema;
 
 let Friend = new Schema({
   user: {
-    type: User,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-  },
-  friendStatus: {
-    type: Number,
-    enums: [
-      0, //Add a friend,
-      1, //Requested as a friend
-      2, //Pending friend request
-      3, //Friend
-    ],
+    default: null,
   },
   friendedDate: {
     type: Date,
--- server/routes/api/friend.js
+++ server/routes/api/friend.js
@@ -6,17 +6,17 @@ const Friend = require("../../models/friend.model");
 
 // Returns array of Friend Object IDs
 friendRoutes.get("/friends-list", (req, res) => {
-  User.db.collections.users.find({googleId: req.body.googleId}).toArray((error, doc) => {
-    console.log(doc[0].friendlist);
-    res.status(200).json(doc[0].friendlist);
+  User.find({googleId: req.body.googleId}, (error, docs) => {
+    console.log(docs[0].friendlist);
+    res.status(200).json(docs[0].friendlist);
   });
 });
 
 friendRoutes.get("/friend-info", (req, res) => {
   console.log(req.body.id);
-  Friend.db.collections.friends.find({_id: req.body.id}).toArray((error, doc) => {
-    console.log(doc[0]);
-    User.db.collections.users.find({_id: doc[0].user}).toArray((smallError, smallDoc) => {
+  Friend.find({_id: req.body.id}, (error, docs) => {
+    console.log(docs);
+    User.find({_id: docs[0].user}, (smallError, smallDoc) => {
       console.log(smallDoc[0]);
       res.status(200).json(smallDoc[0]);
     });
@@ -24,9 +24,10 @@ friendRoutes.get("/friend-info", (req, res) => {
 });
 
 friendRoutes.post("/new", (req, res) => {
-  Friend.db.collections.friends.insertOne(req.body).then((doc) => {
-    console.log(doc);
-    res.status(200).json(doc);
+  let newFriend = new Friend(req.body);
+  newFriend.save().then((addedFriend) => {
+    console.log(addedFriend);
+    res.status(200).json({ addedFriend });
   });
 });
 
