6ba37b956664b0636e6848ce8d33bb4d3c78f795
tony <48603440+antonybstack@users.noreply.github.com>
2021-05-03 17:20:17 -0400

    personal profile info display updated

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,67 +1,80 @@
-import React, { useState } from "react";
-import axios from "axios";
-import moment from "moment-timezone";
+import React from "react";
+import { useAtom } from "jotai";
+import { userAtom } from "../atoms/AtomHelpers";
+import { Divider } from "antd";
 
 function Profile(props) {
-  const [email, setEmail] = useState("");
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
-  const [image, setImage] = useState("");
-  const [userId, setUserId] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [first, setFirst] = useState("");
+  // const [last, setLast] = useState("");
+  // const [image, setImage] = useState("");
+  // const [userId, setUserId] = useState("");
+  const [user] = useAtom(userAtom);
 
-  axios
-    .post("/api/profile/spec", { googleId: props.googleId })
-    .then((res) => {
-      setEmail(res.data.email);
-      setFirst(res.data.firstName);
-      setLast(res.data.lastName);
-      setImage(res.data.googleImg);
-      setUserId(res.data._id);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // axios
+  //   .post("/api/profile/spec", { googleId: props.googleId })
+  //   .then((res) => {
+  //     setEmail(res.data.email);
+  //     setFirst(res.data.firstName);
+  //     setLast(res.data.lastName);
+  //     setImage(res.data.googleImg);
+  //     setUserId(res.data._id);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 
-  const addNewFriend = (event) => {
-    event.preventDefault();
-    let date = moment().tz("America/New_York");
-    axios.post("/api/friend/new", {
-      user: userId,
-      friendedDate: date,
-      onlineStatus: true,
-    });
-  };
+  // const addNewFriend = (event) => {
+  //   event.preventDefault();
+  //   let date = moment().tz("America/New_York");
+  //   axios.post("/api/friend/new", {
+  //     user: userId,
+  //     friendedDate: date,
+  //     onlineStatus: true,
+  //   });
+  // };
 
   return (
-    <div className="event-create-container">
-      <div className="event-header">
-        <div className="event-header-middle">Profile</div>
-        <div className="event-close-outside" onClick={props.handleClose}>
-          <div className="event-close-x-left">
-            <div className="event-close-x-right"></div>
-          </div>
-        </div>
+    <>
+      <h2 className="site-description-item-profile-p" style={{ marginBottom: 24 }}>
+        User Profile
+      </h2>
+      <Divider />
+      <p className="site-description-item-profile-p">Personal</p>
+      <div className="profile-pic">
+        <img src={user.googleImg} alt={"Profile"} />
       </div>
-      <div className="event-form">
-        <div className="profile-pic">
-          <img src={image} alt={"Profile"} />
-        </div>
+      <div>
         <div className="profile-title">User:</div>
         <div className="profile-text">
-          {first} {last}
+          {user.firstName} {user.lastName}
         </div>
-        {props.loggedInProfile ? (
-          <>
-            <div className="profile-title">Email:</div>
-            <div className="profile-text">{email}</div>{" "}
-          </>
-        ) : (
-          <button className="add-friend" onClick={addNewFriend}>
-            Add friend
-          </button>
-        )}
       </div>
-    </div>
+      <div>
+        <div className="profile-title">Email:</div>
+        <div className="profile-text">{user.email}</div>
+      </div>
+    </>
+    // <div className="event-create-container">
+    //   <div className="event-header">
+    //     <div className="event-header-middle">Profile</div>
+    //     <div className="event-close-outside" onClick={props.handleClose}>
+    //       <div className="event-close-x-left">
+    //         <div className="event-close-x-right"></div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="event-form">
+
+    //     {props.loggedInProfile ? (
+
+    //     ) : (
+    //       <button className="add-friend" onClick={addNewFriend}>
+    //         Add friend
+    //       </button>
+    //     )}
+    //   </div>
+    // </div>
   );
 }
 
--- app/src/components/ProfileButton.js
+++ app/src/components/ProfileButton.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Profile from "./Profile";
 import { useAtom } from "jotai";
 import { userAtom } from "../atoms/AtomHelpers";
+import { Drawer, List, Avatar, Divider, Col, Row } from "antd";
 
 function ProfileButton() {
   const [user] = useAtom(userAtom);
+  const [drawerVisible, setDrawerVisible] = useState(false);
 
   var style = {
     backgroundImage: "url(" + user.googleImg + ")",
@@ -23,10 +25,20 @@ function ProfileButton() {
     });
   };
 
+  const toggleDrawer = () => {
+    setDrawerVisible(true);
+  };
+
+  const onClose = () => {
+    setDrawerVisible(false);
+  };
+
   return (
     <div>
-      <div>{displayProfile.displayProfile ? <Profile googleId={user.googleId} handleClose={ProfileClick} loggedInProfile={true} /> : null}</div>
-      <div className="nav-profile" style={style} onClick={ProfileClick}></div>
+      <div className="nav-profile" style={style} onClick={toggleDrawer}></div>
+      <Drawer width={300} placement="right" closable={false} onClose={onClose} visible={drawerVisible}>
+        <Profile googleId={user.googleId} handleClose={ProfileClick} loggedInProfile={true} />
+      </Drawer>
     </div>
   );
 }
