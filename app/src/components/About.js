3e604bcb7433fd23abf3011046cca116373d1c7d
Matthew Rossi <12902926+MattRossi@users.noreply.github.com>
2021-05-07 00:00:50 -0400

    Added info buttons to profile drawer

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -1,62 +1,37 @@
-import React, { useState } from "react";
-import { Modal } from "antd";
+import React from "react";
 import logo from "../assets/logo512.png";
 
 const About = () => {
 
-    const [visible, setVisible] = useState(false);
-
-    const toggleVisible = () => {
-        setVisible(!visible);
-    };
-
     return (
         <>
-            <div className="links" onClick={toggleVisible}>
-                About
+            <div className="aboutModal">
+                <img id="aboutLogo" src={logo} alt={"GlobeChats logo"} />
+                <span id="logoName" className="aboutName">GlobeChats</span>
             </div>
-            <Modal
-                title="About GlobeChats"
-                centered
-                visible={visible}
-                onOk={() => setVisible(false)}
-                onCancel={() => setVisible(false)}
-                footer={null}
-            >
-                <div className="aboutModal">
-                    <img id="aboutLogo" src={logo} alt={"GlobeChats logo"} />
-                    <span id="logoName" className="aboutName">GlobeChats</span>
-                </div>
+
+            <br />
+
+            <div className="aboutModalCenter">
+                <p>GlobeChats is a location-based chat room application that is tailored to specific interests. Users are able to be socially active around nearby individuals from the comfort of their home. GlobeChats also allows users to create and plan in-person events for those nearby to meet up safely.</p>
+
+                <br />
+                <h2>Developers</h2>
+                <p>Matthew Rossi (Project Manager)</p>
+                <p>Antony Blyakher (Lead Developer)</p>
+                <p>Joshua Boyer</p>
+                <p>Roderick Keys</p>
+                <p>Tony Lee</p>
+                <p>Anna Tsambounieris</p>
 
                 <br />
 
-                <div className="aboutModalCenter">
-                    <p>GlobeChats is a location-based chat room application that is tailored to specific interests. Users are able to be socially active around nearby individuals from the comfort of their home. GlobeChats also allows users to create and plan in-person events for those nearby to meet up safely.</p>
-
-                    <br />
-                    <h2>Developers</h2>
-                    <p>Matthew Rossi (Project Manager)</p>
-                    <p>Antony Blyakher (Lead Developer)</p>
-                    <p>Joshua Boyer</p>
-                    <p>Roderick Keys</p>
-                    <p>Tony Lee</p>
-                    <p>Anna Tsambounieris</p>
-
-                    <br />
-
-                    <h2>Technologies</h2>
-                    <p>Mongoose, MongoDB, React JS, Express, Node.js</p>
-                    <p>Mapbox, Google Identity, Cryptr, Ant Design</p>
-                </div>
-
-                
-
-
-
-            </Modal>
+                <h2>Technologies</h2>
+                <p>Mongoose, MongoDB, React JS, Express, Node.js</p>
+                <p>Mapbox, Google Identity, Cryptr, Ant Design</p>
+            </div>
         </>
     );
-
 };
 
 export default About;
--- app/src/components/ProfileButton.js
+++ app/src/components/ProfileButton.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import Profile from "./Profile";
 import { useAtom } from "jotai";
 import { userAtom } from "../atoms/AtomHelpers";
-import { Drawer } from "antd";
+import { Drawer, Button, Modal } from "antd";
+import About from "./About";
+import FAQ from "./FAQ";
+import TermsOfService from "./TermsOfService";
+import Privacy from "./Privacy";
 
 function ProfileButton() {
   const [user] = useAtom(userAtom);
   const [drawerVisible, setDrawerVisible] = useState(false);
+  const [aboutVisible, setAboutVisible] = useState(false);
+  const [faqVisible, setFaqVisible] = useState(false);
+  const [tosVisible, setTosVisible] = useState(false);
+  const [privacyVisible, setPrivacyVisible] = useState(false);
 
   var style = {
     backgroundImage: "url(" + user.googleImg + ")",
@@ -26,7 +34,44 @@ function ProfileButton() {
   return (
     <div>
       <div className="nav-profile" style={style} onClick={toggleDrawer}></div>
-      <Drawer width={"30%"} placement="right" closable={false} onClose={onClose} visible={drawerVisible}>
+      <Drawer width={"30%"} placement="right" closable={false} onClose={onClose} visible={drawerVisible} push={null} footer={<div
+              style={{
+                textAlign: 'center',
+              }}
+            >
+              <Button onClick={() => setAboutVisible(!aboutVisible)} style={{ marginRight: 8 }} type="secondary">
+                About
+                <Modal
+                title="About GlobeChats"
+                centered
+                visible={aboutVisible}
+                footer={null}>
+                  <About />
+                </Modal>
+              </Button>
+              <Button onClick={() => setFaqVisible(!faqVisible)} style={{ marginRight: 8 }} type="secondary">
+                FAQ
+                <Modal
+                title="About GlobeChats"
+                centered
+                visible={faqVisible}
+                footer={null}>
+                  <FAQ />
+                </Modal>
+              </Button>
+              <Button onClick={() => setTosVisible(!tosVisible)} style={{ marginRight: 8 }} type="secondary">
+                Terms of Service
+                <Drawer height={"100%"} placement="bottom" closable={true} visible={tosVisible}>
+                  <TermsOfService />
+                </Drawer>
+              </Button>
+              <Button onClick={() => setPrivacyVisible(!privacyVisible)} style={{ marginRight: 8 }} type="secondary">
+                Privacy Policy
+                <Drawer height={"100%"} placement="bottom" closable={true} visible={privacyVisible}>
+                  <Privacy />
+                </Drawer>
+              </Button>
+            </div>}>
         <Profile />
       </Drawer>
     </div>
